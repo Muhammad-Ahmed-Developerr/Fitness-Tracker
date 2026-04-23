b327c1b6186bab57e37bcc5b33e1a33b28e1e8e4
Anas Zahid <mr.anas1003@gmail.com>
2026-04-24 01:58:02 +0500

    add sender to track the notifier

--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const notificationSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null }, // Null means System
   message: { type: String, required: true },
   type: { type: String, enum: ['Workout', 'Goal', 'Reminder', 'System', 'Nutrition', 'Progress', 'Profile', 'Support'], default: 'System' },
   read: { type: Boolean, default: false },
